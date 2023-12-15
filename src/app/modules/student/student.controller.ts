f23b1b85a5c2acc7441dc1bd1c8ec0ff64cce873
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-12-15 18:51:05 +0600

    deleted createstudent from ftudent controller

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,23 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { StudentServices } from './student.service';
-import studentValidationSchema from './student.validation';
 
-const createStudent = async (req: Request, res: Response , next : NextFunction) => {
-  try {
-    const { student: studentData } = req.body;
-    const zodParsedData = studentValidationSchema.parse(studentData);
-
-    const result = await StudentServices.createStudentIntoDB(zodParsedData);
-
-    res.status(200).json({
-      success: true,
-      message: 'Student is created succesfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
 
 const pick = <T extends Record<string, unknown>, k extends keyof T>(
   obj: T,
@@ -97,7 +80,6 @@ const deleteStudent = async (req: Request, res: Response , next : NextFunction) 
 };
 
 export const StudentControllers = {
-  createStudent,
   getAllStudents,
   getSingleStudent,
   deleteStudent,
